65c17054dc77194631f28995fafe78af5809d1a9
christianhaitian <mckman@comcast.net>
2020-10-11 16:22:55 -0400

    Update rgb10kerneltest.sh

--- ForThera/rgb10kerneltest.sh
+++ ForThera/rgb10kerneltest.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-sudo wget https://github.com/christianhaitian/rgb10/master/ForThera/BootFileUpdates.tar.gz -a "$LOG_FILE"
-sudo wget https://github.com/christianhaitian/rgb10/master/ForThera/KernelUpdate.tar.gz -a "$LOG_FILE"
-sudo tar --same-owner -zxvf BootFileUpdates.tar.gz -C / | tee -a "$LOG_FILE"
-sudo tar --same-owner -zxvf KernelUpdate.tar.gz -C / | tee -a "$LOG_FILE"
-sudo rm -v BootFileUpdates.tar.gz | tee -a "$LOG_FILE"
-sudo rm -v KernelUpdate.tar.gz | tee -a "$LOG_FILE"
+sudo wget https://github.com/christianhaitian/rgb10/master/ForThera/BootFileUpdates.tar.gz
+sudo wget https://github.com/christianhaitian/rgb10/master/ForThera/KernelUpdate.tar.gz
+sudo tar --same-owner -zxvf BootFileUpdates.tar.gz -C /
+sudo tar --same-owner -zxvf KernelUpdate.tar.gz -C /
+sudo rm -v BootFileUpdates.tar.gz
+sudo rm -v KernelUpdate.tar.gz
 rm -- "$0"
 sudo reboot
