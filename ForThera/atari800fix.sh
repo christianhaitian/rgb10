b93ccaf643185710612243991cd9c77442a5e43d
christianhaitian <mckman@comcast.net>
2020-10-17 15:38:58 -0400

    Update atari800fix.sh

--- ForThera/atari800fix.sh
+++ ForThera/atari800fix.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo wget https://github.com/christianhaitian/rgb10/raw/master/ForThera/Atari800sep.tar
 sudo mv -v /etc/emulationstation/es_systems.cfg /etc/emulationstation/es_systems.cfg.temp101720.bak
-sudo tar -xf Atari800sep.zip /
+sudo tar -xf Atari800sep.tar /
 sudo chown odroid:odroid -R /home/odroid/.config/retroarch/config/Atari800/
 sudo chown odroid:odroid -R /home/odroid/.config/retroarch/config/remaps/Atari800/
 sudo chown odroid:odroid -R /home/odroid/.atari800.cfg
