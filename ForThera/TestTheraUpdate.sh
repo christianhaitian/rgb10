2432db241f110cbc3e3cac457fbd405c22e49b75
christianhaitian <mckman@comcast.net>
2020-10-19 00:07:57 -0400

    Update TestTheraUpdate.sh

--- ForThera/TestTheraUpdate.sh
+++ ForThera/TestTheraUpdate.sh
@@ -287,8 +287,8 @@ sudo rm -v ecwolf.zip | tee -a "$LOG_FILE"
 msgbox "Atari800 fix update have been applied and as an added bonus, you can now run the Wolfenstein 3D port. Hit A to go back to Emulationstation."
 touch "$UPDATE_DONE"
 rm -v -- "$0" | tee -a "$LOG_FILE"
-sudo systemctl restart emulationstation
 tput clear >> /dev/tty1
+sudo systemctl restart emulationstation
 exit 187
 
 else 
