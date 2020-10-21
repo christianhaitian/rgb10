4a922db912527e58a68ca1c539d8ae378c8b290e
christianhaitian <mckman@comcast.net>
2020-10-21 18:33:55 -0400

    Update ogasleepfix.sh

--- ForThera/ogasleepfix.sh
+++ ForThera/ogasleepfix.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo wget https://github.com/christianhaitian/rgb10/tree/master/ForThera/ogasleepfix/sleep -O /usr/lib/systemd/system-sleep/sleep
+sudo https://github.com/christianhaitian/rgb10/raw/master/ForThera/ogasleepfix/sleep -O /usr/lib/systemd/system-sleep/sleep
 sudo chmod 775 /usr/lib/systemd/system-sleep/sleep
 msgbox "OGA 1.1 wifi sleep test fix has been applied.  Your device will now restart."
 sudo reboot
